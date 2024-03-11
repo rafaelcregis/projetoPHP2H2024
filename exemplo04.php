02926c3dfa455a730f55d4f1106a4b44f2f4667b
rafaelcregis <rafael.regis@etec.sp.gov.br>
2024-03-11 20:16:37 -0300

    exemplo 05 php

--- exemplo04.php
+++ exemplo04.php
@@ -6,10 +6,13 @@
 	$soma= $valor1+$valor2;
 
 	echo "A soma de $valor1 mais $valor2 é $soma <br/>";
-
+	// Jeito certo de fazer (Opção 1)
+	
 	echo 'A soma de $valor1 mais $valor2 é $soma <br/>';
+	// Jeito errado de fazer.
 
 	echo 'A soma de ' .  $valor1 . ' mais ' . $valor2 . ' é ' .  $soma . '<br/>';
-
+	// Jeito certo de fazer (Opção 2)
+	// O símbolo "." representa CONCATENAÇÃO.
 
  ?>
