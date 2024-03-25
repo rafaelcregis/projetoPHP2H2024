c1522fe87925fd0a8fd038b74ac9edaf89d5713c
rafaelcregis <rafael.regis@etec.sp.gov.br>
2024-03-25 20:15:01 -0300

    exemplo 10 php

---
+++ exemplo10.php
@@ -0,0 +1,19 @@
+<?php 
+	
+	$nome = 'Rafael';
+	$peso = 80;
+	$altura = 1.70;
+	$altura2 = $altura * $altura;
+	
+	function imc ($peso, $altura,){
+		$imc = $peso ($altura * $altura);
+		return $imc;
+	}
+
+	resp $imc ($peso,$altura);
+	
+
+
+	printf ("Olá $nome, seu peso $peso Kg sua altura $altura seu imc %2f", $resp);
+
+ ?>
