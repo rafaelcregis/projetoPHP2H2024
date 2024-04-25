4d3c72f0c51e00254cb5f949a71e61afc7252210
rafaelcregis <145412855+rafaelcregis@users.noreply.github.com>
2024-04-24 22:17:47 -0300

    Add files via upload

---
+++ exemplo01 (Array) 24_04.php
@@ -0,0 +1,12 @@
+<?php 
+
+$nome = array("Maria", "João", "Pedro", "Paulo", "Lucas");
+	echo 'Os nomes no array </br>';
+
+
+foreach( $nome as $nomes ){
+
+	echo "<br/>" . $nomes;
+}
+
+?>
---
+++ exemplo02 (Array) 24_04.php
@@ -0,0 +1,12 @@
+<?php 
+
+$num = array(1, 2, 3, 4, 5);
+	echo 'Os números no array </br>';
+
+
+foreach( $num as $valor ){
+
+	echo "<br/> Valor é $valor";
+}
+
+?>
