cd0ce9bbb9e3fe51ffb3cf6c05499f08d1fdd4db
rafaelcregis <145412855+rafaelcregis@users.noreply.github.com>
2024-05-20 20:20:54 -0300

    Add files via upload

---
+++ Exercicio02 20_05.php
@@ -0,0 +1,27 @@
+<?php 
+
+$num = array (10, -5, 8, 11, -4, -17, 14, 18, -21);
+
+for ($i=0; $i < 10; $i++) { 
+}
+
+$negativos = 0;
+$positivos = 0;
+$pares = 0;
+$impares = 0;
+
+  	foreach ($num as $numeros) {
+  		if ($numero < 0)
+  		echo "<br/> Foram digitados "$negativos" negativos<br/>"; {
+  			$negativos++;
+  		} elseif ($numero >= 0) {
+  			$positivos++;
+  		}
+  		if ($numero % 2 == 0) {
+  			$pares
+  		}
+  	  		
+  	}
+ 
+  
+ ?>
