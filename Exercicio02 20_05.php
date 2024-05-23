f646d9e3fd0a2412840c19dc4727b5f8ca929023
rafaelcregis <145412855+rafaelcregis@users.noreply.github.com>
2024-05-22 22:00:37 -0300

    Add files via upload

--- Exercicio02 20_05.php
+++ Exercicio02 20_05.php
@@ -1,6 +1,8 @@
 <?php 
 
-$num = array (10, -5, 8, 11, -4, -17, 14, 18, -21);
+//Rafael Luz e Rafael Carvalhais
+
+$num = array (10, -5, 8, 11, -4, -17, 14, 18, -21, -3);
 
 for ($i=0; $i < 10; $i++) { 
 }
@@ -10,18 +12,21 @@ $positivos = 0;
 $pares = 0;
 $impares = 0;
 
-  	foreach ($num as $numeros) {
-  		if ($numero < 0)
-  		echo "<br/> Foram digitados "$negativos" negativos<br/>"; {
-  			$negativos++;
-  		} elseif ($numero >= 0) {
-  			$positivos++;
-  		}
-  		if ($numero % 2 == 0) {
-  			$pares
-  		}
-  	  		
-  	}
+    foreach ($num as $numero) {
+      if ($numero < 0) {
+        $negativos++;
+    }   elseif ($numero > 0) {
+          $positivos++;
+    }
  
-  
+      if ($numero % 2 == 0) {
+        $pares++;
+    }  else {
+          $impares++;
+    }
+}
+    echo "<br/>Quantidade de números negativos: " . $negativos;
+  	echo "<br/>Quantidade de números positivos: " . $positivos;
+    echo "<br/>Quantidade de números paress: " . $pares;
+    echo "<br/>Quantidade de números impares: " . $impares;
  ?>
